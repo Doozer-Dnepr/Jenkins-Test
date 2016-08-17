2a702070b3e81eacb2eb02f6577558c2176e066b
Oleksii Gerasymov <Oleksii_Gerasymov@epam.com>
2016-08-17 16:43:28 +0300

    Tests added

---
+++ src/test/java/com/epam/LittleTest.java
@@ -0,0 +1,14 @@
+package com.epam;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class LittleTest {
+
+    @Test
+    public void SimpleTest() {
+        int numberOne = 1;
+        int numberTwo = 1;
+        assertEquals(numberOne, numberTwo);
+    }
+}
